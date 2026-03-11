64fae2e0e9224fd00328552f3010b4dbd5a62c60
damienmiri <damienmiri@gmail.com>
2026-03-11 08:29:09 +0000

    Configure Cloudflare adapter and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // Par défaut 'static'. Changez en 'server' si vous avez besoin de SSR.
+  adapter: cloudflare({
+    mode: 'directory',
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
