d6eff9b9daa2c4fb37f70d8c29d81ee00e247e6a
damienmiri <damienmiri@gmail.com>
2026-03-12 08:14:31 +0000

    Simplify astro config for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // Par défaut 'static'. Changez en 'server' si vous avez besoin de SSR.
-  adapter: cloudflare({
-    mode: 'directory',
-  }),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
